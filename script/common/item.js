5db82d4e5d1aeded7f70bab383007f5fa16b7606
RHSvenson <konto@rhsvenson.dk>
2023-01-13 00:13:21 +0100

    Revert "Started work on shatterable armour"
    This reverts commit 94a280ee8a3d010af47da15cf0cb66e930f811ce.

--- script/common/item.js
+++ script/common/item.js
@@ -267,8 +267,6 @@ export class DarkHeresyItem extends Item {
 
   get isAdditive() { return this.system.isAdditive; }
 
-  get isShatterable() { return this.system.isShatterable; }
-
   get craftsmanship() { return this.system.craftsmanship;}
 
   get description() { return this.system.description;}
